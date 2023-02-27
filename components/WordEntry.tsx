5c393dffec18aaa0fba7d53b6423b25f053a92e5
peron <peron_ap@icloud.com>
2023-02-27 19:50:39 +0800

    style: make text smaller

--- components/WordEntry.tsx
+++ components/WordEntry.tsx
@@ -2,12 +2,12 @@ export function WordEntryRow({ word }: { word: MarugotoWord }) {
   return (
     <div class="flex items-center justify-between p-4 bg-white rounded-lg shadow-lg">
       <div class="flex flex-col mr-4">
-        <p class="text-2xl font-bold leading-tight">{word.kana}</p>
+        <p class="text-xl font-bold leading-tight">{word.kana}</p>
         <p class="text-lg font-medium text-gray-500 leading-tight">
           {word.romaji}
         </p>
       </div>
-      <div class="text-lg font-medium text-gray-500 text-right">
+      <div class="text-xl font-medium text-gray-500 text-right">
         <p>{word.english}</p>
       </div>
     </div>
