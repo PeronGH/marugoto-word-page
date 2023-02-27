deb5a615e987bf18c060c2f769c3e705ca218b3f
peron <peron_ap@icloud.com>
2023-02-27 23:11:07 +0800

    fix: no shadow on ios safari

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,6 +5,7 @@ export function SearchBar(
     <div class="flex items-center justify-center">
       <div class="flex-1">
         <input
+          style="-webkit-appearance: none;"
           class="w-full px-4 py-2 text-lg text-gray-700 bg-white rounded-full shadow-md focus:outline-none focus:shadow-outline"
           type="text"
           placeholder="Search..."
